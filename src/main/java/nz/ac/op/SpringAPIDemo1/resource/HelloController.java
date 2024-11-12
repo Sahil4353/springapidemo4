6a7efa0a8eaa07fc50132912693de839c3566437
Sahil Kakadiya <125628877+Sahil4353@users.noreply.github.com>
2024-11-12 01:41:03 +0000

    testing re dploy

--- src/main/java/nz/ac/op/SpringAPIDemo1/resource/HelloController.java
+++ src/main/java/nz/ac/op/SpringAPIDemo1/resource/HelloController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @GetMapping("/")
+    public String welcome(){
+        return "Welcome to Spring boot REST API";
+    }
     @GetMapping("/hello")
     public String sayHello(){
         return "Welcome to Spring boot REST API";
